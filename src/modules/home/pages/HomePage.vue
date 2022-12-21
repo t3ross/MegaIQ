f3faf843752017932f90c97c539a13dbd51fe43a
AndressWalt <julianwalteros415263@gmail.com>
2022-12-21 11:30:29 -0500

    Home Content: layout drawers design finished

--- src/modules/home/pages/HomePage.vue
+++ src/modules/home/pages/HomePage.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- ----------Header---------- -->
+
   <q-header class="bg-blur">
     <q-toolbar class="bg-blur">
       <q-toolbar-title> MegaIQ </q-toolbar-title>
@@ -11,20 +13,131 @@
     MegaIQ
   </div>
 
-  <div class="layout-container flex justify-center q-pa-lg">
-    <q-layout view="hHh lpr fFf" class="content-layout flex align-center">
+  <!-- ----------Main---------- -->
+
+  <div class="layout-container flex justify-center q-pt-md">
+    <q-layout view="hHh lpr fFf" class="layout-content flex align-center">
+      <!-- ----Left drawer---- -->
+
       <q-drawer
         side="left"
         :width="250"
         :breakpoint="500"
-        content-class="bg-grey-3"
-        class="bg-indigo-4"
+        content-style="laterals"
         show-if-above
       >
-        <p>Friends</p>
+        <div class="laterals left-lateral">
+          <p
+            class="q-px-md q-pt-md q-mb-none text-h5 text-white text-weight-regular"
+          >
+            Filtrar resultados
+          </p>
+
+          <!-- Type checkboxes -->
+
+          <div class="q-pa-md">
+            <p class="q-mb-xs text-subtitle1 text-white text-weight-regular">
+              Tipo
+            </p>
+
+            <q-option-group
+              v-model="group"
+              :options="filterType"
+              color="indigo-4"
+              type="checkbox"
+              class="filter-checkboxes q-ml-auto q-mr-md"
+            />
+
+            <!-- Features Checkboxes -->
+
+            <p
+              class="q-pt-lg q-mb-xs text-subtitle1 text-white text-weight-regular"
+            >
+              Características
+            </p>
+            <q-option-group
+              v-model="group"
+              :options="filterFeatures"
+              color="indigo-4"
+              type="checkbox"
+              class="filter-checkboxes q-ml-auto q-mr-md"
+            />
+
+            <!-- Subjects checkboxes -->
+
+            <p
+              class="q-pt-lg q-mb-xs text-subtitle1 text-white text-weight-regular"
+            >
+              Asignaturas
+            </p>
+            <q-option-group
+              v-model="group"
+              :options="filterSubjects"
+              color="indigo-4"
+              type="checkbox"
+              class="filter-checkboxes q-ml-auto q-mr-md"
+            />
+          </div>
+        </div>
       </q-drawer>
 
-      <q-page-container
+      <!-- ----Right drawer---- -->
+
+      <q-drawer
+        side="right"
+        :width="250"
+        :breakpoint="500"
+        content-style="laterals"
+        show-if-above
+      >
+        <div class="laterals right-lateral q-pa-md">
+          <p class="text-h5 text-white text-weight-regular">Amigos</p>
+
+          <q-list class="friend-list">
+            <q-item
+              clickable
+              v-ripple:white
+              class="no-padding friend-item"
+              :class="{ 'last-friend': friend.id === '3' }"
+              v-for="friend in friendsList"
+              :key="friend.id"
+            >
+              <q-item-section avatar>
+                <q-avatar size="56px">
+                  <img :src="friend.pfp" />
+                  <div
+                    class="online-dot"
+                    v-if="friend.status === 'active'"
+                  ></div>
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section
+                class="friend-info text-white"
+                :class="{ 'first-friend': friend.id === '1' }"
+              >
+                {{ friend.name }}
+                <br />
+
+                <span class="friend-last-msg text-weight-light text-subtitle1">
+                  <i
+                    :class="
+                      friend.msgSeen
+                        ? 'fa-solid fa-circle-check'
+                        : 'fa-regular fa-circle-check'
+                    "
+                  ></i>
+                  {{ friend.lastMsg }}</span
+                >
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+      </q-drawer>
+
+      <!-- ----Main content---- -->
+
+      <q-page-container class=""
         ><p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
           inventore nemo fugit dolorum asperiores neque odit facere laboriosam,
@@ -32,17 +145,6 @@
           dignissimos nam!
         </p></q-page-container
       >
-
-      <q-drawer
-        side="right"
-        :width="250"
-        :breakpoint="500"
-        content-class="bg-grey-3"
-        class="bg-indigo-4"
-        show-if-above
-      >
-        <p>Filter results</p>
-      </q-drawer>
     </q-layout>
   </div>
 </template>
@@ -56,27 +158,109 @@ export default defineComponent({
   components: {},
 
   setup() {
-    const leftDrawerOpen = ref(false);
-
     return {
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
+      group: ref(['op1']),
+
+      filterType: [
+        {
+          label: 'Blog',
+          value: 'blog',
+        },
+        {
+          label: 'Video',
+          value: 'video',
+        },
+      ],
+
+      filterFeatures: [
+        {
+          label: 'Más recientes',
+          value: 'mostRecent',
+        },
+        {
+          label: 'Mejor votados',
+          value: 'bestVoted',
+        },
+        {
+          label: 'Más comentados',
+          value: 'mostCommented',
+        },
+        {
+          label: 'Más antiguos',
+          value: 'older',
+        },
+      ],
+      filterSubjects: [
+        {
+          label: 'Matemáticas',
+          value: 'math',
+        },
+        {
+          label: 'Biología',
+          value: 'biology',
+        },
+        {
+          label: 'Química',
+          value: 'chemistry',
+        },
+        {
+          label: 'Física',
+          value: 'physics',
+        },
+        {
+          label: 'Geografía',
+          value: 'geography',
+        },
+        {
+          label: 'Artes',
+          value: 'art',
+        },
+        {
+          label: 'Economía',
+          value: 'economy',
+        },
+      ],
+      friendsList: [
+        {
+          name: 'Michael',
+          pfp: 'https://cdn.quasar.dev/img/boy-avatar.png',
+          id: '1',
+          status: 'active',
+          lastMsg: 'Brooo',
+          msgSeen: true,
+          lastMsgDate: '12:30',
+        },
+        {
+          name: 'Andrés Walteros',
+          pfp: 'https://i.pinimg.com/736x/2d/ef/82/2def8264083dab1a4788e0d0e3b47c8d.jpg',
+          id: '2',
+          status: 'active',
+          lastMsg: 'Holaa',
+          msgSeen: false,
+          lastMsgDate: '',
+        },
+        {
+          name: 'Valen',
+          pfp: 'https://i.redd.it/ij5qucry9mk81.jpg',
+          id: '3',
+          status: 'inactive',
+          lastMsg: 'Suba al gato',
+          msgSeen: true,
+          lastMsgDate: '',
+        },
+      ],
     };
   },
 });
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .bg-blur {
-  background-color: rgba(29, 31, 158, 0.267);
   backdrop-filter: blur(15px);
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.164);
+  background-color: rgba($primary, 0.2);
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
 }
 .header-banner {
-  margin-top: -50px;
-  height: 70vh;
   background-image: linear-gradient(
       180deg,
       rgba(31, 50, 219, 0.747) 10%,
@@ -84,9 +268,68 @@ export default defineComponent({
     ),
     url('https://4.bp.blogspot.com/-u7xOINjjawk/XHKV0l9lc8I/AAAAAAAAC0Y/RR3HQuyUIjsPqkPhwPTnGfA1f582YPvlgCKgBGAs/w0/blue-red-smoke-abstract-4k-53.jpg');
   background-size: cover;
+  height: 70vh;
+  margin-top: -50px;
 }
-.content-layout {
+.layout-content {
   max-width: 1600px;
-  padding: 20px;
+}
+.laterals {
+  background: linear-gradient(
+    133deg,
+    rgba(121, 135, 203, 0.7) 0%,
+    rgba(52, 88, 255, 0.7) 100%
+  );
+  width: 100%;
+}
+
+.left-lateral {
+  border-radius: 0 8px 8px 0;
+}
+.filter-checkboxes {
+  background-color: $white;
+  border-radius: 8px;
+  padding: 8px 0;
+}
+.filter-checkboxes:last-child {
+  margin-bottom: 12px;
+}
+
+.right-lateral {
+  border-radius: 8px 0 0 8px;
+}
+.friend-list {
+  border-top: 1px solid rgba($white, 0.719);
+
+  .friend-item {
+    height: 76px;
+  }
+  .friend-item:last-child {
+    height: 76px;
+  }
+  .friend-info {
+    border-top: 1px solid rgba($white, 0.719);
+    line-height: 20px;
+    font-size: 18px;
+  }
+  .first-friend {
+    border-top: none;
+    color: red;
+  }
+
+  .online-dot {
+    width: 12px;
+    height: 12px;
+    background: #99cc00;
+    outline: 3px solid $primary;
+    position: absolute;
+    bottom: 2%;
+    right: 2%;
+    border-radius: 100%;
+  }
+  .last-friend {
+    border-bottom: 1px solid rgba($white, 0.719);
+    margin-bottom: 12px;
+  }
 }
 </style>
