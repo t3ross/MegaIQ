05088584fd5c3427e5506a07f2b85401f7d60bfb
AndressWalt <julianwalteros415263@gmail.com>
2022-12-28 11:14:12 -0500

    Home Content: filter buttons design

--- src/modules/home/pages/HomePage.vue
+++ src/modules/home/pages/HomePage.vue
@@ -9,14 +9,20 @@
     </q-toolbar>
   </q-header>
 
-  <div class="text-h2 main-font header-banner flex flex-center text-white">
-    MegaIQ
+  <div
+    class="text-h1 header-font header-banner column q-gutter-y-xl flex-center text-white">
+    <div>MegaIQ</div>
+    <p class="header-text text-body1 text-font-medium">
+      Repasa conceptos, aprende cosas nuevas y comparte con demás estudiantes e
+      instructores sobre temas académicos. MegaIQ está diseñado para
+      complementar y mejorar tu aprendizaje escolar!
+    </p>
   </div>
 
   <!-- ----------Main---------- -->
 
-  <div class="layout-container flex justify-center q-pt-md">
-    <q-layout view="hHh lpr fFf" class="layout-content flex align-center">
+  <div class="layout-container flex q-py-md">
+    <q-layout view="hHh lpr fFf" class="layout-content flex">
       <!-- ----Left drawer---- -->
 
       <q-drawer
@@ -45,31 +51,24 @@
               type="checkbox"
               class="filter-checkboxes q-ml-auto q-mr-md" />
 
-            <!-- Features Checkboxes -->
-
-            <p
-              class="q-pt-lg q-mb-xs text-subtitle1 text-white text-weight-regular">
-              Características
-            </p>
-            <q-option-group
-              v-model="group"
-              :options="filterFeatures"
-              color="indigo-4"
-              type="checkbox"
-              class="filter-checkboxes q-ml-auto q-mr-md" />
-
             <!-- Subjects checkboxes -->
 
             <p
               class="q-pt-lg q-mb-xs text-subtitle1 text-white text-weight-regular">
               Asignaturas
             </p>
+
             <q-option-group
               v-model="group"
               :options="filterSubjects"
               color="indigo-4"
               type="checkbox"
               class="filter-checkboxes q-ml-auto q-mr-md" />
+
+            <q-btn
+              color="secondary"
+              label="Aplicar"
+              class="apply-btn q-mt-md" />
           </div>
         </div>
       </q-drawer>
@@ -82,7 +81,7 @@
         :breakpoint="500"
         content-style="laterals"
         show-if-above>
-        <div class="laterals right-lateral q-pa-md">
+        <div class="laterals right-lateral q-pa-md relative-position">
           <p class="text-h5 text-white text-weight-regular">Amigos</p>
 
           <q-list class="friend-list">
@@ -125,58 +124,103 @@
 
       <!-- ----Main content---- -->
 
-      <q-page-container class="main full-height">
-        <div class="main-container">
-          <div
-            class="main-post q-pa-md border"
-            v-for="post in mainPosts"
-            :key="post.postId">
-            <div class="post-header"></div>
-            <q-toolbar class="post-title text-h5 q-px-none">
-              <q-toolbar-title class="column justify-between">
-                <div class="q-pt-sm">
-                  {{ post.postTitle }} -
-                  <span class="post-author vertical-middle text-subtitle1">
-                    <q-avatar size="20px" class="q-mb-xs"
-                      ><img
-                        :src="post.postAuthorPfp"
-                        alt="{{ post.postAuthor }} profile image"
-                    /></q-avatar>
-                    {{ post.postAuthor }}</span
-                  >
-                </div>
-                <div class="post-description text-grey-8 q-pt-sm">
-                  {{ post.postDescription }}
-                </div>
-              </q-toolbar-title>
-
-              <div class="post- flex q-gutter-md">
-                <div class="post-like-count column flex-center">
-                  <q-btn
-                    dense
-                    flat
-                    round
-                    icon="fa-solid fa-heart fa-lg"
-                    size="20px"
-                    class="text-indigo-2" />
-                  {{ post.postLikeCount }}
-                </div>
-                <div class="post-dislike-count column flex-center">
-                  <q-btn
-                    dense
-                    flat
-                    round
-                    icon="fa-solid fa-heart-crack fa-lg"
-                    size="20px"
-                    class="text-indigo-2" />
-                  {{ post.postDislikeCount }}
-                </div>
+      <q-page-container class="main full-height flex">
+        <!-- <q-scroll-area class="main-container q-gutter-y-md fit"> -->
+        <div
+          class="selCont-container full-width row flex-center q-pa-md white-border">
+          <q-btn
+            :ripple="{ early: false, color: 'indigo-3', keyCodes: [] }"
+            class="selCont-option col column flex-center selCont-top white-border"
+            stack
+            unelevated>
+            <span class="fa-solid fa-fire"></span>
+            Top
+          </q-btn>
+          <q-btn
+            :ripple="{ early: false, color: 'indigo-3', keyCodes: [] }"
+            class="selCont-option col column flex-center selCont-recent white-border"
+            stack
+            unelevated>
+            <span class="fa-solid fa-star"></span>
+            Recientes
+          </q-btn>
+          <q-btn
+            :ripple="{ early: false, color: 'indigo-3', keyCodes: [] }"
+            class="selCont-option col column flex-center selCont-voted white-border"
+            stack
+            unelevated>
+            <span class="fa-solid fa-heart"></span>
+            Más votos
+          </q-btn>
+          <q-btn
+            :ripple="{ early: false, color: 'indigo-3', keyCodes: [] }"
+            class="selCont-option col column flex-center selCont-more white-border"
+            stack
+            unelevated>
+            <span class="fa-solid fa-compass"></span>
+            Descubrir
+          </q-btn>
+        </div>
+        <div
+          class="main-post q-pa-md border white-border"
+          v-for="post in mainPosts"
+          :key="post.postId">
+          <div class="post-header"></div>
+          <q-toolbar class="post-title text-h5 q-px-none">
+            <q-toolbar-title class="column justify-between">
+              <div class="q-pt-sm">
+                {{ post.postTitle }} -
+                <span class="post-author vertical-middle text-subtitle1">
+                  <q-avatar size="20px" class="q-mb-xs"
+                    ><img
+                      :src="post.postAuthorPfp"
+                      alt="{{ post.postAuthor }} profile image"
+                  /></q-avatar>
+                  {{ post.postAuthor }}</span
+                >
               </div>
-            </q-toolbar>
+              <div class="post-description text-grey-8 q-pt-sm">
+                {{ post.postDescription }}
+              </div>
+            </q-toolbar-title>
 
-            <q-separator spaced />
+            <div class="flex q-gutter-md q-pr-sm">
+              <div class="post-like-count column flex-center">
+                <q-btn
+                  dense
+                  flat
+                  round
+                  icon="fa-solid fa-heart fa-lg"
+                  size="16px"
+                  class="text-indigo-2" />
+                {{ post.postLikeCount }}
+              </div>
+              <div class="post-dislike-count column flex-center">
+                <q-btn
+                  dense
+                  flat
+                  round
+                  icon="fa-solid fa-heart-crack fa-lg"
+                  size="16px"
+                  class="text-indigo-2" />
+                {{ post.postDislikeCount }}
+              </div>
+            </div>
+          </q-toolbar>
+
+          <q-separator class="blog-header-separator q-mt-md" dark />
+
+          <div class="row flex-center">
+            <img
+              :src="post.postImage"
+              width="530"
+              class="bg-grey-1 q-my-lg q-pa-sm" />
+            <div>
+              {{ post.postContentFilter() }}
+            </div>
           </div>
         </div>
+        <!-- </q-scroll-area> -->
       </q-page-container>
     </q-layout>
   </div>
@@ -191,8 +235,75 @@ export default defineComponent({
   components: {},
 
   setup() {
+    const mainPosts = [
+      {
+        postId: '1',
+        postTitle: 'Cómo pasar trigo',
+        postAuthor: 'Andres Walteros',
+        postAuthorPfp:
+          'https://i.etsystatic.com/34732889/r/il/b08942/3768265623/il_570xN.3768265623_sji1.jpg',
+        postDate: '1 month ago',
+        postLikeCount: '23',
+        postDislikeCount: '2',
+        postImage:
+          'https://i0.wp.com/lasmatesfaciles.com/wp-content/uploads/2019/10/intro-trigonometria.png?fit=840%2C473&ssl=1',
+        postDescription:
+          'Guía básica para entender los conceptos principales de trigonometría.',
+        postContent:
+          'Anim mollit proident deserunt est excepteur reprehenderit eu cupidatat id cupidatat. Voluptate dolore tempor laboris nostrud proident quis Lorem eiusmod. Duis veniam proident velit cillum ut irure velit excepteur do ipsum. Sint consectetur tempor eiusmod deserunt laboris. In consectetur Lorem nulla culpa. Est dolore cupidatat aute eiusmod in consectetur incididunt fugiat est nulla ea adipisicing.',
+        postContentFilter: function () {
+          return this.postContent.length > 200
+            ? this.postContent.substring(0, 200) + '...'
+            : this.postContent;
+        },
+      },
+      {
+        postId: '1',
+        postTitle: 'Cómo pasar trigo',
+        postAuthor: 'Andres Walteros',
+        postAuthorPfp:
+          'https://i.etsystatic.com/34732889/r/il/b08942/3768265623/il_570xN.3768265623_sji1.jpg',
+        postDate: '1 month ago',
+        postLikeCount: '23',
+        postDislikeCount: '2',
+        postImage:
+          'https://i0.wp.com/lasmatesfaciles.com/wp-content/uploads/2019/10/intro-trigonometria.png?fit=840%2C473&ssl=1',
+        postDescription:
+          'Guía básica para entender los conceptos principales de trigonometría.',
+        postContent:
+          'Anim mollit proident deserunt est excepteur reprehenderit eu cupidatat id cupidatat. Voluptate dolore tempor laboris nostrud proident quis Lorem eiusmod. Duis veniam proident velit cillum ut irure velit excepteur do ipsum. Sint consectetur tempor eiusmod deserunt laboris. In consectetur Lorem nulla culpa. Est dolore cupidatat aute eiusmod in consectetur incididunt fugiat est nulla ea adipisicing.',
+        postContentFilter: function () {
+          return this.postContent.length > 200
+            ? this.postContent.substring(0, 200) + '...'
+            : this.postContent;
+        },
+      },
+      {
+        postId: '1',
+        postTitle: 'Cómo pasar trigo',
+        postAuthor: 'Andres Walteros',
+        postAuthorPfp:
+          'https://i.etsystatic.com/34732889/r/il/b08942/3768265623/il_570xN.3768265623_sji1.jpg',
+        postDate: '1 month ago',
+        postLikeCount: '23',
+        postDislikeCount: '2',
+        postImage:
+          'https://i0.wp.com/lasmatesfaciles.com/wp-content/uploads/2019/10/intro-trigonometria.png?fit=840%2C473&ssl=1',
+        postDescription:
+          'Guía básica para entender los conceptos principales de trigonometría.',
+        postContent:
+          'Anim mollit proident deserunt est excepteur reprehenderit eu cupidatat id cupidatat. Voluptate dolore tempor laboris nostrud proident quis Lorem eiusmod. Duis veniam proident velit cillum ut irure velit excepteur do ipsum. Sint consectetur tempor eiusmod deserunt laboris. In consectetur Lorem nulla culpa. Est dolore cupidatat aute eiusmod in consectetur incididunt fugiat est nulla ea adipisicing.',
+        postContentFilter: function () {
+          return this.postContent.length > 200
+            ? this.postContent.substring(0, 200) + '...'
+            : this.postContent;
+        },
+      },
+    ];
+
     return {
       group: ref(['op1']),
+      mainPosts,
 
       filterType: [
         {
@@ -205,24 +316,6 @@ export default defineComponent({
         },
       ],
 
-      filterFeatures: [
-        {
-          label: 'Más recientes',
-          value: 'mostRecent',
-        },
-        {
-          label: 'Mejor votados',
-          value: 'bestVoted',
-        },
-        {
-          label: 'Más comentados',
-          value: 'mostCommented',
-        },
-        {
-          label: 'Más antiguos',
-          value: 'older',
-        },
-      ],
       filterSubjects: [
         {
           label: 'Matemáticas',
@@ -282,22 +375,6 @@ export default defineComponent({
           lastMsgDate: '',
         },
       ],
-      mainPosts: [
-        {
-          postId: '1',
-          postTitle: 'Cómo pasar trigo',
-          postImage:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Trigonometria_02.svg/1200px-Trigonometria_02.svg.png',
-          postDescription:
-            'Guía básica para entender los conceptos principales de trigonometría.',
-          postAuthor: 'Andres Walteros',
-          postAuthorPfp:
-            'https://i.etsystatic.com/34732889/r/il/b08942/3768265623/il_570xN.3768265623_sji1.jpg',
-          postDate: '1 month ago',
-          postLikeCount: '23',
-          postDislikeCount: '2',
-        },
-      ],
     };
   },
 });
@@ -306,7 +383,7 @@ export default defineComponent({
 <style scoped lang="scss">
 //----------------Header
 .bg-blur {
-  backdrop-filter: blur(15px);
+  backdrop-filter: blur(10px);
   background-color: rgba($primary, 0.2);
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
 }
@@ -318,13 +395,20 @@ export default defineComponent({
     ),
     url('https://4.bp.blogspot.com/-u7xOINjjawk/XHKV0l9lc8I/AAAAAAAAC0Y/RR3HQuyUIjsPqkPhwPTnGfA1f582YPvlgCKgBGAs/w0/blue-red-smoke-abstract-4k-53.jpg');
   background-size: cover;
-  height: 70vh;
+  height: 90vh;
   margin-top: -50px;
 }
+.header-text {
+  width: 800px;
+}
 
+#header-video::-webkit-media-controls {
+  display: none;
+}
 //----------------Main
 .layout-content {
   max-width: 1600px;
+  min-height: 860px !important;
 }
 
 //----Laterals
@@ -335,19 +419,24 @@ export default defineComponent({
     rgba(52, 88, 255, 0.7) 100%
   );
   width: 100%;
+  position: fixed;
 }
 
 //Left-lateral
 .left-lateral {
   border-radius: 0 8px 8px 0;
-}
-.filter-checkboxes {
-  background-color: $white-blue;
-  border-radius: 8px;
-  padding: 8px 0;
-}
-.filter-checkboxes:last-child {
-  margin-bottom: 12px;
+
+  .filter-checkboxes {
+    background-color: $white-blue;
+    border-radius: 8px;
+    padding: 8px 0;
+  }
+  .filter-checkboxes:last-child {
+    margin-bottom: 12px;
+  }
+  .apply-btn {
+    width: 200px;
+  }
 }
 
 //Right-lateral
@@ -392,17 +481,30 @@ export default defineComponent({
 //-----Main
 
 .main {
-  padding: 16px !important;
+  padding: 0 !important;
   margin: 0 266px;
-  border: 1px solid $white;
-  border-radius: 8px;
-  width: 90%;
+
+  gap: 20px;
+
+  .selCont-container {
+    gap: 16px;
+    .selCont-option {
+      width: 200px;
+      padding: 10px;
+    }
+    .selCont-option span {
+      font-size: 28px;
+      padding: 4px 0 12px 0;
+      color: rgba($primary, 0.5);
+    }
+    .selCont-option:hover {
+      background: rgba($info, 0.1);
+    }
+  }
 }
 
 //-----Posts
 
-.post-author {
-}
 .post-description {
   font-size: 14px;
 }
@@ -412,8 +514,7 @@ export default defineComponent({
   background: $white;
   border-radius: 6px;
 }
-.main-post {
-  border: 1px solid $light-grey;
-  border-radius: 8px;
+.blog-header-separator {
+  background: $white;
 }
 </style>
