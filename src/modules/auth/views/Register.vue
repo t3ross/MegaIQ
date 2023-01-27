a3ab4759418175fb672b799bee7d23ed7979a5c0
AndressWalt <julianwalteros415263@gmail.com>
2023-01-27 16:53:14 -0500

    Test before adding Vuedilate

--- src/modules/auth/views/Register.vue
+++ src/modules/auth/views/Register.vue
@@ -18,21 +18,27 @@
       type="email"
       v-model="state.email"
       label="Tu correo electrónico"
+      :error-message="validations('email')"
+      :error="v$.email.$invalid" />
+    <!-- <q-input
+      class="auth-input"
+      filled
+      type="email"
+      v-model="state.email"
+      label="Tu correo electrónico"
       lazy-rules
       :rules="[
         (val) =>
           (val !== null && val !== '') ||
           'Por favor escribe tu correo electrónico',
-        (val) =>
-          (val > 0 && val < 1) ||
-          'Por favor escribe un correo electrónico válido',
-      ]" />
+        (val) => val > 0 || 'Por favor escribe un correo electrónico válido',
+      ]" /> -->
 
     <q-input
       class="auth-input"
       filled
-      :type="isPwd ? 'password' : 'text'"
-      v-model="state.password"
+      :type="isPwd1 ? 'password' : 'text'"
+      v-model="state.password.password"
       label="Escribe una  contraseña"
       lazy-rules
       :rules="[
@@ -41,28 +47,38 @@
         (val) =>
           val.length >= 8 ||
           'Por favor escribe una contraseña con mínimo 8 carácteres.',
+        //   val.value.match(letterNumber) ||
+        //   'Por favor escribe por lo menos un número y una letra',
         (val) =>
-          val.includes('') ||
+          v$.password.minLength ||
           'Por favor escribe por lo menos un número y una letra',
       ]">
-      <template v-slot:append v-if="password">
+      <template v-slot:append v-if="state.password.password">
         <q-icon
-          :name="isPwd ? 'visibility_off' : 'visibility'"
+          :name="isPwd1 ? 'visibility_off' : 'visibility'"
           class="cursor-pointer"
-          @click="isPwd = !isPwd" />
+          @mousedown="isPwd1 = !isPwd1"
+          @mouseup="isPwd1 = !isPwd1" />
       </template>
     </q-input>
 
     <q-input
       class="auth-input"
       filled
-      type="password"
-      v-model="passwordCheck"
+      :type="isPwd2 ? 'password' : 'text'"
+      v-model="state.password.confirm"
       label="Repite la contraseña"
       lazy-rules
       :rules="[
         (val) => (val && val.length > 0) || 'Por favor escribe algún nombre',
-      ]" />
+      ]"
+      ><template v-slot:append v-if="state.password.confirm">
+        <q-icon
+          :name="isPwd2 ? 'visibility_off' : 'visibility'"
+          class="cursor-pointer"
+          @mousedown="isPwd2 = !isPwd2"
+          @mouseup="isPwd2 = !isPwd2" /> </template
+    ></q-input>
 
     <q-toggle
       v-model="state.accept"
@@ -88,7 +104,13 @@
 <script>
 import { useQuasar } from 'quasar';
 import { useVuelidate } from '@vuelidate/core';
-import { required, email, alphaNum } from '@vuelidate/validators';
+import {
+  alphaNum,
+  email,
+  minLength,
+  required,
+  sameAs,
+} from '@vuelidate/validators';
 import { ref } from 'vue';
 
 export default {
@@ -99,21 +121,47 @@ export default {
     const state = ref({
       username: '',
       email: '',
-      password: '',
+      password: {
+        password: '',
+        confirm: '',
+      },
       accept: false,
     });
 
     const rules = {
-      username: { required },
+      username: { required, minLength: minLength(3) },
       email: { required, email },
-      password: { required, alphaNum },
+      password: {
+        password: { required, minLength: minLength(8), alphaNum },
+        confirm: { required, sameAs: sameAs(state.value.password.password) },
+      },
     };
 
     const v$ = useVuelidate(rules, state);
 
+    console.log(v$.value.email.$invalid);
+
+    const validations = (field) => {
+      if (field === 'username') {
+        if (v$.value.email.required) return 'Por favor escribe un nombre';
+        if (v$.value.email.minLength)
+          return 'Por favor escribe un nombre con mínimo 3 carácteres.';
+      }
+      if (field === 'email') {
+        if (v$.value.email.required)
+          return 'Por favor escribe un correo electrónico';
+        if (v$.value.email.email)
+          return 'Por favor escribe un correo electrónico válido';
+      }
+    };
+
     return {
+      validations,
+      isPwd1: ref(true),
+      isPwd2: ref(true),
       state,
       v$,
+      letterNumber: ref(/^[0-9a-zA-Z]+$/),
 
       onSubmit() {
         if (accept.value !== true) {
